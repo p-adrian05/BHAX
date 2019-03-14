7d57abb7b704c8da111e34c856c1bafe1e6d1efe
Adrian <adrian16616@gmail.com>
2019-03-14 11:07:43 +0100

    neural

--- attention_raising/NN_R/nn.r
+++ attention_raising/NN_R/nn.r
@@ -66,7 +66,7 @@ EXOR    <- c(0,1,1,0)
 
 exor.data <- data.frame(a1, a2, EXOR)
 
-nn.exor <- neuralnet(EXOR~a1+a2, exor.data, hidden=c(6, 4, 6), linear.output=FALSE, stepmax = 1e+07, threshold = 0.000001)
+nn.exor <- neuralnet(EXOR~a1+a2, exor.data, hidden=2, linear.output=FALSE, stepmax = 1e+07, threshold = 0.000001)
 
 plot(nn.exor)
 
